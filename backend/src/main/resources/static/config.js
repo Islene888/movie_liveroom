4716076551fa1f309bda87271701fb7e069dcdd7
Ella Zhao <146047825+Islene888@users.noreply.github.com>
2025-06-25 17:58:47 -0700

    Update config.js

--- backend/src/main/resources/static/config.js
+++ backend/src/main/resources/static/config.js
@@ -6,6 +6,6 @@
 
 // 上线时把地址改成云服务器的地址
 window.APP_CONFIG = {
-    WS_API_BASE_URL: "ws://34.134.39.193",
+    WS_API_BASE_URL: "ws://34.134.39.193:8080",
     HTTP_API_BASE_URL: "http://34.134.39.193" // fetch 请求
 };
